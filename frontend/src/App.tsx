931aa4b2eb244739656f18e3abde6d29c4aa0218
lnirjar <36673870+lnirjar@users.noreply.github.com>
2025-01-24 15:35:56 +0530

    Changed Theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,10 @@ import { Button } from "@/components/ui/button";
 function App() {
   return (
     <div>
-      <h1 className="text-9xl font-bold">Hello</h1>
-      <Button>Click me</Button>
+      <div className="text-center mt-4">
+        <h1 className="text-9xl font-bold">Hello</h1>
+        <Button className="mt-4">Click me</Button>
+      </div>
     </div>
   );
 }
