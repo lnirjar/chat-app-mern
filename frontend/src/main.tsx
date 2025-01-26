717250b8daeed19a2da5592dca42233f2edbcd0c
lnirjar <36673870+lnirjar@users.noreply.github.com>
2025-01-26 14:01:12 +0530

    Toggle Theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,6 +8,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 
 import store from "@/config/store";
+import { ThemeProvider } from "@/components/theme-provider.tsx";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,7 +23,9 @@ createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
       <BrowserRouter>
         <QueryClientProvider client={queryClient}>
-          <App />
+          <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <App />
+          </ThemeProvider>
         </QueryClientProvider>
       </BrowserRouter>
     </Provider>
